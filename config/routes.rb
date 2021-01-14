d874342a2347f39b99674159d1aa204a53caf112
jomapormentilla <jomapormentilla@gmail.com>
2021-01-14 00:42:35 -0500

    commit tree 058585b449939b912513ef4fdb950e24e1d8a6ac

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # Add your routes here
+  match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
+  root 'welcome#home'
+  get '/logout' => 'sessions#destroy', as: 'logout'
 end
